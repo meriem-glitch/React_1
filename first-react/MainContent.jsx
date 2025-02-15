21c5b7ad2807d9e4980ce0519008ef4fa8d9004c
meriem <meriembelaid44@gmail.com>
2025-02-15 09:56:07 +0100

    Ajout des nouvelles modifications

---
+++ first-react/MainContent.jsx
@@ -0,0 +1,11 @@
+
+export default function MainContent(){
+    return(
+       <main>
+       <ol>
+          <li>React is popular library, so I will be able to fit in with all the coolest devs out there! 😊</li>
+          <li>I am more likely to get a job as a front end developer if I know React</li>
+       </ol>
+       </main>
+    )
+ }
